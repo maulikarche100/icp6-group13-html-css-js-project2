6e0cc5dd5a35491cf3d59a790f38b8cc5794c297
mauli karche <maulikarche36@gmail.com>
2023-12-14 21:21:29 +0530

    first commit

---
+++ js/blog.js
@@ -0,0 +1,28 @@
+
+
+  
+        // This is a simple JavaScript code block
+        // It displays a message in the browser console
+        console.log("Hello from JavaScript!");
+
+        // Variables
+        let greeting = "Welcome to JavaScript!";
+        alert(greeting);
+
+        // Functions
+        function sayHello(name) {
+            alert("Hello, " + name + "!");
+        }
+
+        // Call the function
+        sayHello("John");
+
+        // Conditional statement
+        let number = 10;
+        if (number > 5) {
+            console.log("The number is greater than 5");
+        } else {
+            console.log("The number is not greater than 5");
+        }
+    
+
