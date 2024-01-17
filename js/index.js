967b7d60e4d8e29709770e5c1dbb102dcab64da2
mauli karche <maulikarche36@gmail.com>
2024-01-17 14:02:15 +0530

    .

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ showSlides();
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
+    setTimeout(showSlides, 2000); // Change image every 3 seconds
 }
     
 
